ad9cca8472f6f8a13e912b2949ef86c0eae60551
znbflyingant <zhounianbin@52tt.com>
2025-09-09 11:17:18 +0800

    Update PM2 configuration for production and testing environments; add new build scripts for test deployment in Vue frontend.

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,20 +1,38 @@
 module.exports = {
   apps: [
     {
-      name: 'firmware-server',
+      name: 'firmware-server-prod',
       script: 'index.js',
       instances: 1,
       exec_mode: 'fork',
       watch: false,
       env: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        PORT: 3001
       },
       env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        PORT: 3001
       },
       max_memory_restart: '300M',
       error_file: './logs/err.log',
       out_file: './logs/out.log',
+      merge_logs: true,
+      time: true
+    },
+    {
+      name: 'firmware-server-test',
+      script: 'index.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      env: {
+        NODE_ENV: 'test',
+        PORT: 3101
+      },
+      max_memory_restart: '300M',
+      error_file: './logs/err-test.log',
+      out_file: './logs/out-test.log',
       merge_logs: true,
       time: true
     }
